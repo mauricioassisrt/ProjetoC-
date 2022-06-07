c9da54b1252436da043e2bae012195e7fadb368b
Mauricio de Assis <mauriciox8mm@hotmail.com>
2022-06-06 22:43:48 -0300

    ajustes nas entity

---
+++ ControleContatos/ControleContatos/Models/ContatoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ControleContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public string Contato { get; set; }
+    }
+}
