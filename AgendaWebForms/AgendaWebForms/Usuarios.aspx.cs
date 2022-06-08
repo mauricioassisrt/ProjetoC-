225d8e16f75ee61068a3313f5264f818fb1e24b2
Mauricio de Assis <mauriciox8mm@hotmail.com>
2022-06-08 20:56:42 -0300

    ajustes nas msg de insert

---
+++ AgendaWebForms/AgendaWebForms/Usuarios.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AgendaWebForms
+{
+    public partial class Usuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSourceUsuarios_Inserted(object sender, SqlDataSourceStatusEventArgs e)
+        {
+            if (e.Exception != null)
+            {
+                lMsg.Text = "Erro não foi possivel inserir o dado";
+                e.ExceptionHandled = true;
+            }
+        }
+
+        protected void SqlDataSourceUsuarios_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSourceUsuarios_Updating(object sender, SqlDataSourceCommandEventArgs e)
+        {
+           
+        }
+
+        protected void SqlDataSourceUsuarios_Updated(object sender, SqlDataSourceStatusEventArgs e)
+        {
+            if (e.Exception != null)
+            {
+                lMsg.Text = "Erro não foi possivel atualizar o dado";
+                e.ExceptionHandled = true;
+            }
+        }
+    }
+}
